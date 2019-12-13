93139081f3435bd674e8117a135baa441c67685f
mikibaka <kaliszewski.mikolaj@gmail.com>
2019-12-13 01:15:21 +0100

    fixed bug from last commit by negating selectAreAlbumsLoaded

--- src/pages/albums/albums.container.jsx
+++ src/pages/albums/albums.container.jsx
@@ -1,20 +1,17 @@
-import React from 'react';
-
 import { connect } from 'react-redux';
 import { compose } from 'redux'
 import { createStructuredSelector } from 'reselect';
 import { selectAreAlbumsLoaded } from '../../redux/albums/albums.selectors';
-import WithSpinner from '../../with-spinner';
+import WithSpinner from '../../with-spinner/with-spinner.component';
 import AlbumsPage from './albums.page'
 
 const mapStateToProps = createStructuredSelector({
-    isLoading: selectAreAlbumsLoaded
+    isLoading: state => !selectAreAlbumsLoaded(state)
 })
 
 const AlbumsPageContainer = compose(
     connect(mapStateToProps),
     WithSpinner
-
 )(AlbumsPage)
 
 
--- src/pages/songs/songs.container.jsx
+++ src/pages/songs/songs.container.jsx
@@ -1,18 +1,16 @@
-import React from 'react';
-
 import { connect } from 'react-redux';
 import { compose } from 'redux'
 import { createStructuredSelector } from 'reselect';
 import { selectAreAlbumsLoaded } from '../../redux/albums/albums.selectors';
-import WithSpinner from '../../with-spinner';
+import WithSpinner from '../../with-spinner/with-spinner.component';
 import SongsPage from './songs.page'
 
 //In this way we are wrapping HOC (withSpinner) with connect, so we get right props into WithSpinner without
 //having to pass it as a props in App.js.
 
-
+//we use state as a negation, since cSS passes state
 const mapStateToProps = createStructuredSelector({
-    isLoading: selectAreAlbumsLoaded
+    isLoading: state => !selectAreAlbumsLoaded(state)
 })
 
 //compose curring it all. From right to left => conect(mapStateToProps)(WithSpinner(SongsPage))
