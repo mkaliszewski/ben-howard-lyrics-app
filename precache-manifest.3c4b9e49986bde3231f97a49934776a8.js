1b2a35c299fe35a500db6d97007d8714e8b7d4d0
mikibaka <kaliszewski.mikolaj@gmail.com>
2020-01-21 01:35:23 +0100

    Updates

---
+++ precache-manifest.3c4b9e49986bde3231f97a49934776a8.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed81cbacda6470dc2c04d98ae2d9c89f",
+    "url": "/ben-howard-lyrics-app/index.html"
+  },
+  {
+    "revision": "d9f2340e16d78e00b1e7",
+    "url": "/ben-howard-lyrics-app/static/css/10.b972f17d.chunk.css"
+  },
+  {
+    "revision": "5f28f0b4a0e38c2f8ef5",
+    "url": "/ben-howard-lyrics-app/static/css/11.0e77c927.chunk.css"
+  },
+  {
+    "revision": "9980f17950b70e46cf70",
+    "url": "/ben-howard-lyrics-app/static/css/12.ecf718e5.chunk.css"
+  },
+  {
+    "revision": "b69e8a985126a4ff4ae7",
+    "url": "/ben-howard-lyrics-app/static/css/13.5eb61e89.chunk.css"
+  },
+  {
+    "revision": "a87eb3574f99369ed1db",
+    "url": "/ben-howard-lyrics-app/static/css/3.65b547e5.chunk.css"
+  },
+  {
+    "revision": "0f206bfb46324417b836",
+    "url": "/ben-howard-lyrics-app/static/css/4.2b4e470a.chunk.css"
+  },
+  {
+    "revision": "825fb9711eec8671cf0e",
+    "url": "/ben-howard-lyrics-app/static/css/5.0b3b122e.chunk.css"
+  },
+  {
+    "revision": "20b20462f4b83ab86430",
+    "url": "/ben-howard-lyrics-app/static/css/6.af18a568.chunk.css"
+  },
+  {
+    "revision": "c03f4c9a3023418564cb",
+    "url": "/ben-howard-lyrics-app/static/css/7.054102be.chunk.css"
+  },
+  {
+    "revision": "ca38bc51b132081ae18a",
+    "url": "/ben-howard-lyrics-app/static/css/8.8f99dab2.chunk.css"
+  },
+  {
+    "revision": "8c89a91fe6a4322e2bc7",
+    "url": "/ben-howard-lyrics-app/static/css/9.01e8276b.chunk.css"
+  },
+  {
+    "revision": "793f5722a848bd6f37db",
+    "url": "/ben-howard-lyrics-app/static/css/main.78ba440e.chunk.css"
+  },
+  {
+    "revision": "d9f2340e16d78e00b1e7",
+    "url": "/ben-howard-lyrics-app/static/js/10.f53ce0d5.chunk.js"
+  },
+  {
+    "revision": "5f28f0b4a0e38c2f8ef5",
+    "url": "/ben-howard-lyrics-app/static/js/11.2c0a0663.chunk.js"
+  },
+  {
+    "revision": "9980f17950b70e46cf70",
+    "url": "/ben-howard-lyrics-app/static/js/12.2a51a230.chunk.js"
+  },
+  {
+    "revision": "b69e8a985126a4ff4ae7",
+    "url": "/ben-howard-lyrics-app/static/js/13.846a2081.chunk.js"
+  },
+  {
+    "revision": "f8a585b5357ab8147024",
+    "url": "/ben-howard-lyrics-app/static/js/2.545ec5d6.chunk.js"
+  },
+  {
+    "revision": "8d9df8d43a53af65a9986ea70c2ee5e9",
+    "url": "/ben-howard-lyrics-app/static/js/2.545ec5d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a87eb3574f99369ed1db",
+    "url": "/ben-howard-lyrics-app/static/js/3.67bb5deb.chunk.js"
+  },
+  {
+    "revision": "0f206bfb46324417b836",
+    "url": "/ben-howard-lyrics-app/static/js/4.d4a0ae57.chunk.js"
+  },
+  {
+    "revision": "825fb9711eec8671cf0e",
+    "url": "/ben-howard-lyrics-app/static/js/5.b8377909.chunk.js"
+  },
+  {
+    "revision": "20b20462f4b83ab86430",
+    "url": "/ben-howard-lyrics-app/static/js/6.4505f810.chunk.js"
+  },
+  {
+    "revision": "c03f4c9a3023418564cb",
+    "url": "/ben-howard-lyrics-app/static/js/7.a563dc8f.chunk.js"
+  },
+  {
+    "revision": "ca38bc51b132081ae18a",
+    "url": "/ben-howard-lyrics-app/static/js/8.f2d16aaf.chunk.js"
+  },
+  {
+    "revision": "8c89a91fe6a4322e2bc7",
+    "url": "/ben-howard-lyrics-app/static/js/9.11f59eb0.chunk.js"
+  },
+  {
+    "revision": "793f5722a848bd6f37db",
+    "url": "/ben-howard-lyrics-app/static/js/main.4c59d396.chunk.js"
+  },
+  {
+    "revision": "a6c417d1f7ca3ab85fe2",
+    "url": "/ben-howard-lyrics-app/static/js/runtime-main.1a955be4.js"
+  },
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/ben-howard-lyrics-app/static/media/GitHub_Logo.cefc2023.png"
+  },
+  {
+    "revision": "143221bda95bbdf20fcc5a693034e7d8",
+    "url": "/ben-howard-lyrics-app/static/media/Icon_1.143221bd.svg"
+  },
+  {
+    "revision": "97e1a7a53546cdffa6a55260bd41cf63",
+    "url": "/ben-howard-lyrics-app/static/media/Icon_2.97e1a7a5.svg"
+  },
+  {
+    "revision": "55276807a260f6a59d309c59849f7d8e",
+    "url": "/ben-howard-lyrics-app/static/media/Icon_3.55276807.svg"
+  },
+  {
+    "revision": "69a6a2bdb6ecc17d9b5d3239a181685d",
+    "url": "/ben-howard-lyrics-app/static/media/Icon_4.69a6a2bd.svg"
+  },
+  {
+    "revision": "6f9f98dfecc1ab7b7427eb61330653ad",
+    "url": "/ben-howard-lyrics-app/static/media/Icon_5.6f9f98df.svg"
+  },
+  {
+    "revision": "c24cde2827448f08d40afd6909011e16",
+    "url": "/ben-howard-lyrics-app/static/media/LI-Logo.c24cde28.png"
+  },
+  {
+    "revision": "7394811f9875d2fc8ce984387ce11787",
+    "url": "/ben-howard-lyrics-app/static/media/Logo.7394811f.svg"
+  },
+  {
+    "revision": "a69e59d52725a1f2068af482ae2c75fc",
+    "url": "/ben-howard-lyrics-app/static/media/album_1.a69e59d5.jpg"
+  },
+  {
+    "revision": "df2d1524ec7a5c9486d0f54b50358d00",
+    "url": "/ben-howard-lyrics-app/static/media/album_2.df2d1524.jpg"
+  },
+  {
+    "revision": "41e31f21aa75fe6c928ec5f3c2b1cb31",
+    "url": "/ben-howard-lyrics-app/static/media/album_3.41e31f21.jpg"
+  },
+  {
+    "revision": "f872daa4405a04d256d0cb3b31816349",
+    "url": "/ben-howard-lyrics-app/static/media/error.f872daa4.png"
+  },
+  {
+    "revision": "39233b8de95a13f1ed9ef4792239ea3c",
+    "url": "/ben-howard-lyrics-app/static/media/guitar.39233b8d.png"
+  },
+  {
+    "revision": "fc049ddab8fcc83d083e8be9c34cbfcd",
+    "url": "/ben-howard-lyrics-app/static/media/header.fc049dda.svg"
+  },
+  {
+    "revision": "c5358ff61b843f24c36a784fe48c4f66",
+    "url": "/ben-howard-lyrics-app/static/media/heart.c5358ff6.svg"
+  },
+  {
+    "revision": "c7ace3694c7ac7a03a5c563a5b64923f",
+    "url": "/ben-howard-lyrics-app/static/media/jack.c7ace369.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4d8652f342e2c47dc87a19ead7668693.js"
+  "/ben-howard-lyrics-app/precache-manifest.3c4b9e49986bde3231f97a49934776a8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ben-howard-lyrics-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
